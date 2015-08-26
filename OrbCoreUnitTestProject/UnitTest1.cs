e34b4a9995bf8ce9f2342b482475a41432b2567a
Mike Graham <mgraham@technicalmedia.com>
2015-08-26 10:59:17 -0600

    second unit test - results do not come out nested.  may have to write serializer for that

--- OrbCoreUnitTestProject/UnitTest1.cs
+++ OrbCoreUnitTestProject/UnitTest1.cs
@@ -35,5 +35,54 @@ namespace OrbCoreUnitTestProject
 
             Console.WriteLine(jsonResult);
         }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var dataSet = new DataSet("dataSet");
+            dataSet.Namespace = "NetFrameWork";
+            var table = new DataTable();
+            var idColumn = new DataColumn("id", typeof(int));
+            idColumn.AutoIncrement = true;
+
+            var itemColumn = new DataColumn("item");
+            table.Columns.Add(idColumn);
+            table.Columns.Add(itemColumn);
+            dataSet.Tables.Add(table);
+
+            for (int i = 0; i < 2; i++)
+            {
+                var newRow = table.NewRow();
+                newRow["item"] = "item " + i;
+                table.Rows.Add(newRow);
+            }
+
+            var ctable = new DataTable();
+            var cidColumn = new DataColumn("id", typeof(int));
+            cidColumn.AutoIncrement = true;
+            var cpidColumn = new DataColumn("pid", typeof(int));
+            var citemColumn = new DataColumn("item");
+            ctable.Columns.Add(cidColumn);
+            ctable.Columns.Add(cpidColumn);
+            ctable.Columns.Add(citemColumn);
+            dataSet.Tables.Add(ctable);
+
+            var relation = dataSet.Relations.Add(idColumn, cpidColumn);
+            relation.Nested = true;
+
+            for (int i = 0; i < 4; i++)
+            {
+                var newRow = ctable.NewRow();
+                newRow["item"] = "item " + i;
+                newRow["pid"] = i % 2;
+                ctable.Rows.Add(newRow);
+            }
+
+            dataSet.AcceptChanges();
+
+            var jsonResult = OrbCore.toJson(dataSet);
+
+            Console.WriteLine(jsonResult);
+        }
     }
 }
