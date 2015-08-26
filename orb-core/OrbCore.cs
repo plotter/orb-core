a42861a0fd7ffc821d2692c3e5f2ada11fb3645f
Mike Graham <mgraham@technicalmedia.com>
2015-08-26 09:34:48 -0600

    meager beginnings for the orb :)

---
+++ orb-core/OrbCore.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+using System.Data;
+
+namespace orb_core
+{
+    class OrbCore
+    {
+        string toJson(DataSet dataSet) {
+            return JsonConvert.SerializeObject(dataSet, Formatting.Indented);
+        }
+    }
+}
